a9154ab94714820c4333545f5b3db0d5ad44cc34
Ziomioslaw <dawid.plocki@gmail.com>
2023-10-04 21:44:28 +0200

    Solution 02: Simplified the logic

--- solutions/challenge02.py
+++ solutions/challenge02.py
@@ -2,17 +2,11 @@ from utils import get_file_content
 
 
 def find_sum_of_correct_numbers(tablet: str) -> int:
-    was_number = {}
     content = []
 
     for number in map(int, tablet.split()):
-        if number in was_number:
-            for incorrect_number in content[was_number[number] + 1 :]:
-                del was_number[incorrect_number]
-
-            content = content[: was_number[number]]
-        else:
-            was_number[number] = len(content)
+        if number in content:
+            content = content[:content.index(number)]
 
         content.append(number)
 
