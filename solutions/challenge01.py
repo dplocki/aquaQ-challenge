6f9c4600113064e52edf24ae766b7caae0283e85
Ziomioslaw <dawid.plocki@gmail.com>
2023-10-04 20:01:13 +0200

    Solution 01: Black formatting

--- solutions/challenge01.py
+++ solutions/challenge01.py
@@ -4,19 +4,24 @@ def get_file_content(file_name: str) -> str:
 
 
 def conversion_process(value: str) -> str:
-
     def is_hex(character: str) -> bool:
-        return '0' <= character <= '9' or 'a' <= character <= 'f'
+        return "0" <= character <= "9" or "a" <= character <= "f"
 
     value_size = len(value)
     require_size = (value_size // 3 + 1) * 3
-    extended_value = value.lower() + (require_size - value_size) * '0'
-    characters = list(map(lambda character: character if is_hex(character) else '0', extended_value))
+    extended_value = value.lower() + (require_size - value_size) * "0"
+    characters = list(
+        map(lambda character: character if is_hex(character) else "0", extended_value)
+    )
     characters_size = require_size // 3
 
-    return ''.join(characters[:characters_size][:2] + characters[characters_size:characters_size * 2][:2] + characters[characters_size * 2:][:2])
+    return "".join(
+        characters[:characters_size][:2]
+        + characters[characters_size : characters_size * 2][:2]
+        + characters[characters_size * 2 :][:2]
+    )
 
 
-assert conversion_process('kdb4life') == '0d40fe'
+assert conversion_process("kdb4life") == "0d40fe"
 
-print('Solution', conversion_process(get_file_content('input01.txt')))
+print("Solution", conversion_process(get_file_content("input01.txt")))
