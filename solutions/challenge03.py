b291da719be998d4b598494fb53f01cfa6b87efd
Ziomioslaw <dawid.plocki@gmail.com>
2023-10-05 20:48:17 +0200

    Solution 03: Refactoring

--- solutions/challenge03.py
+++ solutions/challenge03.py
@@ -21,23 +21,31 @@ def text_room_map_converter(text_map: [str]) -> dict:
 
 def calculate_new_position(position: Tuple[int, int], instruction) -> Tuple[int, int]:
     if instruction == "U":
-        return (position[0] - 1, position[1])
+        return position[0] - 1, position[1]
 
     if instruction == "D":
-        return (position[0] + 1, position[1])
+        return position[0] + 1, position[1]
 
     if instruction == "L":
-        return (position[0], position[1] - 1)
+        return position[0], position[1] - 1
 
     if instruction == "R":
-        return (position[0], position[1] + 1)
+        return position[0], position[1] + 1
 
     raise Exception(f"Unknown instruction {instruction}")
 
 
-def walk(room_map: set, instructions: str) -> int:
-    position = (0, 2)
+def find_start_position(room_map: set) -> Tuple[int, int]:
+    row_index = column_index = len(room_map)
 
+    for current_row_index, column_index in room_map:
+        column_index = min(column_index, current_row_index)
+        row_index = min(row_index, current_row_index)
+
+    return row_index, column_index
+
+
+def walk(room_map: set, position: Tuple[int, int], instructions: str) -> int:
     for instruction in instructions:
         new_position = calculate_new_position(position, instruction)
         if new_position in room_map:
@@ -47,9 +55,11 @@ def walk(room_map: set, instructions: str) -> int:
 
 
 def solution(instructions: str) -> int:
+    room_map = text_room_map_converter(TEXT_ROOM_MAP)
+
     return sum(
         column + row
-        for column, row in walk(text_room_map_converter(TEXT_ROOM_MAP), instructions)
+        for column, row in walk(room_map, find_start_position(room_map), instructions)
     )
 
 
