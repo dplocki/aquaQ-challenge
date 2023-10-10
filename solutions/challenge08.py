4bd1ea859bdc49e6cc3c1f1993d931ed8116bd31
dplocki <dawid.plocki@gmail.com>
2023-10-10 22:09:48 +0200

    Solution 08: Base refactoring

--- solutions/challenge08.py
+++ solutions/challenge08.py
@@ -10,26 +10,28 @@ def get_data(content: str) -> Generator[Tuple[str, int, str, int], None, None]:
         yield day_id, int(cells[1]), int(cells[2])
 
 
-own_milk = {}
-own_cereal = 0
+def solution(content):
+    own_milk = {}
+    own_cereal = 0
+
+    for day_id, milk, bought_cereal in get_data(content):
+        own_cereal += bought_cereal
+
+        if own_cereal >= 100 and sum(own_milk.values()) >= 100:
+            own_cereal -= 100
+
+            oldest_milk_key = min(own_milk.keys())
+            own_milk[oldest_milk_key] -= 100
+
+            if own_milk[oldest_milk_key] == 0:
+                del own_milk[oldest_milk_key]
+
+        if day_id - 5 in own_milk:
+            del own_milk[day_id - 5]
+
+        own_milk[day_id] = milk
+
+    return sum(own_milk.values()) + own_cereal
 
 
-for day_id, milk, bought_cereal in get_data(get_file_content('input08.csv')):
-    own_cereal += bought_cereal
-
-    if own_cereal >= 100 and sum(own_milk.values()) >= 100:
-        own_cereal -= 100
-
-        oldest_milk_key = min(own_milk.keys())
-        own_milk[oldest_milk_key] -= 100
-
-        if own_milk[oldest_milk_key] == 0:
-            del own_milk[oldest_milk_key]
-
-    if day_id - 5 in own_milk:
-        del own_milk[day_id - 5]
-
-    own_milk[day_id] = milk
-
-
-print('Solution', sum(own_milk.values()) + own_cereal)
+print("Solution", solution(get_file_content("input08.csv")))
