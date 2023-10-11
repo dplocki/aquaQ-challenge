1c4b7550c6c7cdd5438c6f8f8bc34f5180a86c8d
dplocki <dawid.plocki@gmail.com>
2023-10-11 18:32:07 +0200

    Solution 09: Black formating

--- solutions/challenge09.py
+++ solutions/challenge09.py
@@ -1,14 +1,12 @@
 from functools import reduce
 from operator import mul
-
 from utils import get_file_content
 
 
 def solution(lines: str) -> int:
     return reduce(mul, map(int, lines.splitlines()))
 
-assert solution('''2
-4
-8''') == 64
 
-print('Solution', solution(get_file_content('input09.txt')))
+assert solution("2\n4\n8") == 64
+
+print("Solution", solution(get_file_content("input09.txt")))
