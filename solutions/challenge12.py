12246dcc6f1330458bcb7fd9e8f78e5ca0d647ca
dplocki <dawid.plocki@gmail.com>
2023-10-14 12:42:23 +0200

    Solution 12: Working solution

---
+++ solutions/challenge12.py
@@ -0,0 +1,23 @@
+from utils import get_file_content
+
+
+def solution(content: str) -> int:
+
+    instructions = list(map(lambda x: tuple(map(int, x.split(' '))), content.splitlines()))
+    instructions_max = len(instructions)
+
+    direction = 1
+    floor = 0
+    visited_floors = 1
+
+    while floor >= 0 and floor < instructions_max:
+        direction = direction * -1 if instructions[floor][0] == 0 else direction
+        floor += direction * instructions[floor][1]
+        visited_floors += 1
+
+    return visited_floors
+
+
+assert solution('1 2\n0 3\n1 1\n0 1\n1 5') == 7
+
+print('Solution', solution(get_file_content('input12.txt')))
