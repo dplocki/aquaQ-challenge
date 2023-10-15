97adc697576c6510239f9454ad399d88c3bb8050
dplocki <dawid.plocki@gmail.com>
2023-10-15 20:31:20 +0200

    Solution 13: Black formating

--- solutions/challenge13.py
+++ solutions/challenge13.py
@@ -7,9 +7,14 @@ def floyds_cycle_detection_algorithm(value: str) -> int:
     for index, character in enumerate(value):
         the_next_char_position = value.find(character, index + 1)
         if the_next_char_position > 0:
-            check = value[index:the_next_char_position]
-            if check == value[the_next_char_position:the_next_char_position + the_next_char_position - index]:
-                array.append(len(check))
+            chunk = value[index:the_next_char_position]
+            next_chunk = value[
+                the_next_char_position : the_next_char_position
+                + the_next_char_position
+                - index
+            ]
+            if chunk == next_chunk:
+                array.append(len(chunk))
                 continue
 
         array.append(0)
@@ -17,7 +22,7 @@ def floyds_cycle_detection_algorithm(value: str) -> int:
     index = array.index(max(array))
 
     result = 1
-    chuck = value[index:index + array[index]]
+    chuck = value[index : index + array[index]]
 
     while True:
         if result * chuck in value:
@@ -27,12 +32,8 @@ def floyds_cycle_detection_algorithm(value: str) -> int:
         return result - 1
 
 
-def solution(content: str):
-    return sum(
-        floyds_cycle_detection_algorithm(line)
-        for line in content.splitlines()
-    )
-
+def solution(content: str) -> int:
+    return sum(floyds_cycle_detection_algorithm(line) for line in content.splitlines())
 
 
 assert floyds_cycle_detection_algorithm("BCDEAAHAHAHAHAHXC") == 5
@@ -41,4 +42,4 @@ assert floyds_cycle_detection_algorithm("AAAAAAB") == 6
 assert floyds_cycle_detection_algorithm("ABCDEAAAAAAACCAF") == 7
 
 
-print('Solution', solution(get_file_content("input13.txt")))
+print("Solution", solution(get_file_content("input13.txt")))
