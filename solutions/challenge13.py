c5e36f52fff4cbe2260e753bca246d64b492c990
dplocki <dawid.plocki@gmail.com>
2023-10-15 20:13:26 +0200

    Solution 13: Working solution

--- solutions/challenge13.py
+++ solutions/challenge13.py
@@ -2,21 +2,29 @@ from utils import get_file_content
 
 
 def floyds_cycle_detection_algorithm(value: str) -> int:
+    array = []
+
     for index, character in enumerate(value):
-        if value.count(character, index + 1) == 0:
+        the_next_char_position = value.find(character, index + 1)
+        if the_next_char_position > 0:
+            check = value[index:the_next_char_position]
+            if check == value[the_next_char_position:the_next_char_position + the_next_char_position - index]:
+                array.append(len(check))
+                continue
+
+        array.append(0)
+
+    index = array.index(max(array))
+
+    result = 1
+    chuck = value[index:index + array[index]]
+
+    while True:
+        if result * chuck in value:
+            result += 1
             continue
 
-        the_next_char_position = value.index(value[index], index + 1)
-        check = value[index:the_next_char_position]
-        if check == value[the_next_char_position:the_next_char_position + the_next_char_position - index]:
-            i = 0
-            while True:
-                if i * check not in value:
-                    return i - 1
-
-                i += 1
-
-    return None
+        return result - 1
 
 
 def solution(content: str):
@@ -25,8 +33,12 @@ def solution(content: str):
         for line in content.splitlines()
     )
 
+
+
+assert floyds_cycle_detection_algorithm("BCDEAAHAHAHAHAHXC") == 5
 assert floyds_cycle_detection_algorithm("ABCABCABCABCABC") == 5
 assert floyds_cycle_detection_algorithm("AAAAAAB") == 6
 assert floyds_cycle_detection_algorithm("ABCDEAAAAAAACCAF") == 7
+
 
 print('Solution', solution(get_file_content("input13.txt")))
