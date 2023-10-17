99847accd51ce0c05a98ad4654042c75e86e637c
dplocki <dawid.plocki@gmail.com>
2023-10-17 22:37:00 +0200

    Solution 15: The heap solution

--- solutions/challenge15.py
+++ solutions/challenge15.py
@@ -1,4 +1,5 @@
 from collections import defaultdict, deque
+from heapq import heappop, heappush
 from utils import get_file_content
 
 WORDS = defaultdict(list)
@@ -7,25 +8,22 @@ for word in get_file_content('validwords15.txt').splitlines():
     WORDS[len(word)].append(word)
 
 
+def words_difference(first_word: str, second_word: str) -> int:
+    return sum(0 if f == s else 1 for f, s in zip(first_word, second_word))
+
+
 def is_words_single_characters_difference(first_word: str, second_word: str):
-    flag = False
-    for f, s in zip(first_word, second_word):
-        if f != s:
-            if flag:
-                return False
-
-            flag = True
-
-    return flag
+    return words_difference(first_word, second_word) == 1
 
 
 def find_the_shortest_path(start_word: str, end_word: str) -> int:
     word_size = len(start_word)
 
-    possibilities = deque([ [start_word] ])
+    possibilities = [ ]
+    heappush(possibilities, (words_difference(start_word, end_word), [start_word]))
 
     while possibilities:
-        path = possibilities.popleft()
+        _, path = heappop(possibilities)
         last_word = path[-1]
 
         if last_word == end_word:
@@ -33,7 +31,7 @@ def find_the_shortest_path(start_word: str, end_word: str) -> int:
 
         for word in WORDS[word_size]:
             if is_words_single_characters_difference(last_word, word) and word not in path:
-                possibilities.append(path + [word])
+                heappush(possibilities, (words_difference(word, end_word), path + [word]))
 
     raise Exception('Not found')
 
@@ -53,7 +51,7 @@ assert is_words_single_characters_difference('fly', 'fry') == True
 assert is_words_single_characters_difference('fly', 'try') == False
 assert is_words_single_characters_difference('fly', 'trx') == False
 
-#assert find_the_shortest_path('word', 'maze') == 5
+assert find_the_shortest_path('word', 'maze') == 5
 
 assert solution('fly,try\ntry,fly\nword,maze') == 45
 
