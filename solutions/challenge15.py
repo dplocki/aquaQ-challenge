1f7fed1d3d0ad54a1f960af714e7aa78ef8d1b58
dplocki <dawid.plocki@gmail.com>
2023-10-18 21:35:53 +0200

    Solution 15: Black formatting

--- solutions/challenge15.py
+++ solutions/challenge15.py
@@ -4,7 +4,7 @@ from utils import get_file_content
 
 
 def get_template_for_word(word: str):
-    yield from (word[:i] + '_' + word[i + 1:] for i in range(len(word)))
+    yield from (word[:i] + "_" + word[i + 1 :] for i in range(len(word)))
 
 
 def build_wordbook(content: str) -> Dict[str, list]:
@@ -17,7 +17,9 @@ def build_wordbook(content: str) -> Dict[str, list]:
     return result
 
 
-def find_the_shortest_path(wordbook: Dict[str, list], start_word: str, end_word: str) -> int:
+def find_the_shortest_path(
+    wordbook: Dict[str, list], start_word: str, end_word: str
+) -> int:
     was = set()
     possibilities = deque([(start_word, 0)])
 
@@ -34,22 +36,22 @@ def find_the_shortest_path(wordbook: Dict[str, list], start_word: str, end_word:
                     if not neighbor in was:
                         possibilities.append((neighbor, step + 1))
 
-    raise Exception('Not found')
+    raise Exception("Not found")
 
 
 def solution(wordbook: Dict[str, list], content: str) -> int:
     result = 1
 
     for line in content.splitlines():
-        start_word, end_word = line.split(',')
+        start_word, end_word = line.split(",")
         result *= find_the_shortest_path(wordbook, start_word, end_word)
 
     return result
 
 
-wordbook = build_wordbook(get_file_content('validwords15.txt'))
+wordbook = build_wordbook(get_file_content("validwords15.txt"))
 
-assert find_the_shortest_path(wordbook, 'word', 'maze') == 5
-assert solution(wordbook, 'fly,try\ntry,fly\nword,maze') == 45
+assert find_the_shortest_path(wordbook, "word", "maze") == 5
+assert solution(wordbook, "fly,try\ntry,fly\nword,maze") == 45
 
-print('Solution', solution(wordbook, get_file_content('input15.txt')))
+print("Solution", solution(wordbook, get_file_content("input15.txt")))
