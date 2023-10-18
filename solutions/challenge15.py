0173172163cf48f606d365fbdca87e1c3d0939e3
dplocki <dawid.plocki@gmail.com>
2023-10-18 21:34:52 +0200

    Solution 15: Move build wordbook into function

--- solutions/challenge15.py
+++ solutions/challenge15.py
@@ -1,4 +1,5 @@
 from collections import defaultdict, deque
+from typing import Dict
 from utils import get_file_content
 
 
@@ -6,14 +7,17 @@ def get_template_for_word(word: str):
     yield from (word[:i] + '_' + word[i + 1:] for i in range(len(word)))
 
 
-WORDS = defaultdict(list)
+def build_wordbook(content: str) -> Dict[str, list]:
+    result = defaultdict(list)
 
-for word in get_file_content('validwords15.txt').splitlines():
-    for template in get_template_for_word(word):
-        WORDS[template].append(word)
+    for word in content.splitlines():
+        for template in get_template_for_word(word):
+            result[template].append(word)
+
+    return result
 
 
-def find_the_shortest_path(start_word: str, end_word: str) -> int:
+def find_the_shortest_path(wordbook: Dict[str, list], start_word: str, end_word: str) -> int:
     was = set()
     possibilities = deque([(start_word, 0)])
 
@@ -26,25 +30,26 @@ def find_the_shortest_path(start_word: str, end_word: str) -> int:
                 return step + 1
 
             for template in get_template_for_word(word):
-                for neighbor in WORDS[template]:
+                for neighbor in wordbook[template]:
                     if not neighbor in was:
                         possibilities.append((neighbor, step + 1))
 
     raise Exception('Not found')
 
 
-def solution(content: str) -> int:
+def solution(wordbook: Dict[str, list], content: str) -> int:
     result = 1
 
     for line in content.splitlines():
         start_word, end_word = line.split(',')
-        result *= find_the_shortest_path(start_word, end_word)
+        result *= find_the_shortest_path(wordbook, start_word, end_word)
 
     return result
 
 
-assert find_the_shortest_path('word', 'maze') == 5
+wordbook = build_wordbook(get_file_content('validwords15.txt'))
 
-assert solution('fly,try\ntry,fly\nword,maze') == 45
+assert find_the_shortest_path(wordbook, 'word', 'maze') == 5
+assert solution(wordbook, 'fly,try\ntry,fly\nword,maze') == 45
 
-print('Solution', solution(get_file_content('input15.txt')))
+print('Solution', solution(wordbook, get_file_content('input15.txt')))
