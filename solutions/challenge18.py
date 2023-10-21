7c1cc2cccb4eec55314f3e33924825a904f9668d
dplocki <dawid.plocki@gmail.com>
2023-10-21 13:32:18 +0200

    Solution 18: Add decorator

--- solutions/challenge18.py
+++ solutions/challenge18.py
@@ -1,3 +1,4 @@
+from typing import Callable
 from utils import get_file_content
 
 
@@ -8,25 +9,31 @@ def is_hour_palindromic(hour: int, minute: int, second: int) -> bool:
     return (second % 10 == hour // 10) and (second // 10 == hour % 10)
 
 
-palindromic_hours = [
-    hour * 60 * 60 + minute * 60 + second
-    for hour in range(24)
-    for minute in range(60)
-    for second in range(60)
-    if is_hour_palindromic(hour, minute, second)
-]
-palindromic_hours += [palindromic_hours[0] + 24 * 60 * 60]
+def attach_palindromic_hours(func: Callable) -> Callable:
+    palindromic_hours = [
+        hour * 60 * 60 + minute * 60 + second
+        for hour in range(24)
+        for minute in range(60)
+        for second in range(60)
+        if is_hour_palindromic(hour, minute, second)
+    ]
+    palindromic_hours += [palindromic_hours[0] + 24 * 60 * 60]
 
+    def inner(time_representation):
+        return func(palindromic_hours, time_representation)
+
+    return inner
 
 def str_time_to_seconds(time_representation: str) -> int:
     hour, minute, second = map(int, time_representation.split(":"))
     return hour * 60 * 60 + minute * 60 + second
 
 
-def find_nearest_palindromic(time_representation: str):
-    time = str_time_to_seconds(time_representation)
+@attach_palindromic_hours
+def find_nearest_palindromic(palindromic_hours: list[int], time_representation: str) -> int:
+    requested_time = str_time_to_seconds(time_representation)
 
-    return min(abs(time - t) for t in palindromic_hours)
+    return min(abs(requested_time - hour) for hour in palindromic_hours)
 
 
 def solution(content: str) -> int:
