626e74d8d68061a636a5ca700e3fec5036409c13
dplocki <dawid.plocki@gmail.com>
2023-10-22 15:32:33 +0200

    Solution 00: Black formating, using the get_file_content function, refactoring

--- solutions/challenge00.py
+++ solutions/challenge00.py
@@ -1,4 +1,5 @@
-from typing import Iterator, Tuple
+from typing import Dict, Iterator, Tuple
+from utils import get_file_content
 
 
 KEY_PAD = {
@@ -15,27 +16,28 @@ KEY_PAD = {
 }
 
 
-def load_file_content(file_name: str) -> Iterator[str]:
-    with open(file_name, "r") as file:
-        yield from file.readlines()
-
-
-def read_letter(KEY_PAD, key, times) -> str:
-    letters = KEY_PAD[key]
+def read_letter(key_pad: Dict[int, str], key: int, times: int) -> str:
+    letters = key_pad[key]
 
     return letters[(times - 1) % len(letters)]
 
 
-def parse_input(file_name: str) -> Iterator[Tuple[int, int]]:
-    for line in load_file_content(file_name):
+def parse_input(content: str) -> Iterator[Tuple[int, int]]:
+    for line in content.splitlines():
         yield map(int, line.strip().split())
 
 
-def decode_message(instructions: Iterator[Tuple[int, int]]) -> Iterator[str]:
+def decode_message(
+    key_pad: Dict[int, str], instructions: Iterator[Tuple[int, int]]
+) -> Iterator[str]:
     for key, times in instructions:
-        yield read_letter(KEY_PAD, key, times)
+        yield read_letter(key_pad, key, times)
+
+
+def solution(content: str) -> str:
+    return "".join(decode_message(KEY_PAD, parse_input(content)))
 
 
 assert read_letter(KEY_PAD, 7, 3) == "r"
 
-print("Solution:", "".join(decode_message(parse_input("input00.txt"))))
+print("Solution:", solution(get_file_content("input00.txt")))
