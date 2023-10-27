cfdb87508c9ceccf8dfc1e58317ba120e6867100
dplocki <dawid.plocki@gmail.com>
2023-10-27 20:00:11 +0200

    Solution 24: Refactoring

--- solutions/challenge24.py
+++ solutions/challenge24.py
@@ -13,60 +13,59 @@ class Node:
         return f"{self.value}:{self.frequency}"
 
 
-def pass_by_tree(node, prefix):
-    if node.left == None and node.right == None:
-        yield node.value, prefix
-        return
+def solution(line_for_dictionary: str, encoded_text: str) -> str:
+    def build_tree(text: str):
+        nodes = [Node(letter, frequency) for letter, frequency in Counter(text).items()]
 
-    yield from pass_by_tree(node.left, prefix + "0")
-    yield from pass_by_tree(node.right, prefix + "1")
+        while len(nodes) > 1:
+            nodes.sort(key=lambda node: (node.frequency, len(node.value), node.value))
 
-
-def build_decoding_dictionary(node):
-    return {code: letter for letter, code in pass_by_tree(node, "")}
-
-
-def build_tree(text: str):
-    nodes = [Node(letter, frequency) for letter, frequency in Counter(text).items()]
-
-    while len(nodes) > 1:
-        nodes.sort(key=lambda node: (node.frequency, len(node.value), node.value))
-
-        nodes.append(
-            Node(
-                nodes[0].value + nodes[1].value,
-                nodes[0].frequency + nodes[1].frequency,
-                nodes[0],
-                nodes[1],
+            nodes.append(
+                Node(
+                    nodes[0].value + nodes[1].value,
+                    nodes[0].frequency + nodes[1].frequency,
+                    nodes[0],
+                    nodes[1],
+                )
             )
-        )
 
-        nodes = nodes[2:]
+            nodes = nodes[2:]
 
-    return build_decoding_dictionary(nodes[0])
+        return nodes[0]
 
+    def pass_by_tree(node, prefix):
+        if node.left == None and node.right == None:
+            yield node.value, prefix
+            return
 
-def decoding_message(decode_dictionary, encoded_text: str) -> str:
-    result = []
-    chunk = ""
+        yield from pass_by_tree(node.left, prefix + "0")
+        yield from pass_by_tree(node.right, prefix + "1")
 
-    while encoded_text:
-        chunk += encoded_text[0]
-        encoded_text = encoded_text[1:]
+    def build_decoding_dictionary(node):
+        return {code: letter for letter, code in pass_by_tree(node, "")}
 
-        if chunk in decode_dictionary:
-            result.append(decode_dictionary[chunk])
-            chunk = ""
+    def decoding_message(decode_dictionary, encoded_text: str) -> str:
+        result = []
+        chunk = ""
 
-    return "".join(result)
+        while encoded_text:
+            chunk += encoded_text[0]
+            encoded_text = encoded_text[1:]
+
+            if chunk in decode_dictionary:
+                result.append(decode_dictionary[chunk])
+                chunk = ""
+
+        return "".join(result)
+
+    tree = build_tree(line_for_dictionary)
+    dictionary_tree = build_decoding_dictionary(tree)
+    return decoding_message(dictionary_tree, encoded_text)
 
 
 assert (
-    decoding_message(
-        build_tree("A_DEAD_DAD_CEDED_A_BAD_BABE_A_BEADED_ABACA_BED"), "11101100011000"
-    )
+    solution("A_DEAD_DAD_CEDED_A_BAD_BABE_A_BEADED_ABACA_BED", "11101100011000")
     == "CEDED"
 )
 
-lines = get_file_content("input24.txt").splitlines()
-print("Solution:", decoding_message(build_tree(lines[0]), lines[1]))
+print("Solution:", solution(*get_file_content("input24.txt").splitlines()))
