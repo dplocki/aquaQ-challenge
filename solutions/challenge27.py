70979a36976c9e2af50a601db9af9bd1b918fa2e
dplocki <dawid.plocki@gmail.com>
2023-10-30 20:29:02 +0100

    Solution 27: Not working solution

---
+++ solutions/challenge27.py
@@ -0,0 +1,100 @@
+
+from typing import Dict, Tuple
+from utils import get_file_content
+
+
+def get_neighbors(x, y):
+    yield x + 1, y
+    yield x - 1, y
+    yield x, y + 1
+    yield x, y - 1
+
+
+def parse_content_to_map(content: str) -> Dict[Tuple[int, int], str]:
+    result = {}
+
+    for row_index, row in enumerate(content.splitlines()):
+        for column_index, column in enumerate(row):
+            if column != ' ':
+                result[row_index, column_index] = column
+
+    return result
+
+
+def find_snake_endings(_map):
+    for row, column in _map:
+        neighbor = [
+            (r, c)
+            for r, c in get_neighbors(row, column)
+            if (r,c) in _map]
+
+        if len(neighbor) == 1:
+            yield row, column
+
+
+def only_valid_neighbors(_map, point):
+    yield from (p
+        for p in get_neighbors(*point)
+        if p in _map)
+
+
+def walk(was: set, _map, start_point):
+    if start_point in was:
+        return
+
+    next_point = next(only_valid_neighbors(_map, start_point))
+    direction = next_point[0] - start_point[0], next_point[1] - start_point[1]
+    current_point = start_point
+
+    result = ''
+
+    while True:
+        result += _map[current_point]
+        was.add(current_point)
+        new_current_point = current_point[0] + direction[0], current_point[1] + direction[1]
+
+        if new_current_point not in _map:
+            neighbors = [n for n in only_valid_neighbors(_map, current_point) if n not in was]
+            if len(neighbors) == 0:
+                yield result
+                return
+
+            next_point = neighbors[0]
+            direction = next_point[0] - current_point[0], next_point[1] - current_point[1]
+
+            yield result
+            result = ''
+        else:
+            current_point = new_current_point
+
+
+def solution(content: str):
+    _map = parse_content_to_map(content)
+    letter_a = ord('a')
+
+    result = 0
+    was = set()
+    for start in sorted(find_snake_endings(_map), key=lambda coordinates: coordinates[1]):
+        for word in walk(was, _map, start):
+            result += sum(ord(letter) - letter_a + 1 for letter in word) * len(word)
+
+    return result
+
+
+test_input ='''                roulette
+                e      l
+                v      e
+                e      c
+                netulg t
+    invalidly        n i
+            a        i o
+            c        y n
+            h        r sharpness
+            t        r
+            i        u
+            n        c
+            grumpiness'''
+
+assert solution(test_input) == 7995
+
+print('Solution', solution(get_file_content('input27.txt')))
