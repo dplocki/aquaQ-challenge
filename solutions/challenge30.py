e30edf3cbef53f48c056959710bded4450fdb3ea
dplocki <dawid.plocki@gmail.com>
2023-11-03 21:58:35 +0100

    Solution 30: Working solution

--- solutions/challenge30.py
+++ solutions/challenge30.py
@@ -1,54 +1,12 @@
-from functools import lru_cache
-from typing import Tuple
 from utils import get_file_content
 
 
-def get_possible_splits_of_deck(deck: str) -> Tuple[str, str]:
-    index = deck.find('1', 0)
-    while index > - 1:
-        yield split_the_deck(deck, index)
-        index = deck.find('1', index + 1)
-
-
-@lru_cache
-def can_win(deck: str) -> bool:
-    if deck == None:
-        return True
-
-    if deck == '1':
-        return True
-
-    if deck.count('0') == len(deck):
-        return False
-
-    for part_generator in get_possible_splits_of_deck(deck):
-        if not can_win(next(part_generator, None)):
-            continue
-
-        if not can_win(next(part_generator, None)):
-            continue
-
-        return True
-
-    return False
-
-
-def flip_card(card: str) -> str:
-    return '1' if card == '0' else '0'
-
-
-def split_the_deck(deck: str, index: int):
-    if index > 0:
-        yield deck[:index - 1] + flip_card(deck[index - 1])
-
-    if index + 1 < len(deck):
-        yield flip_card(deck[index + 1]) + deck[index + 2:]
-
-
 def count_winning_positions(deck: str) -> int:
-    return sum(1
-        for part_generator in get_possible_splits_of_deck(deck)
-        if all(map(can_win, part_generator)))
+    ones = deck.count('1')
+    if ones % 2 == 1:
+        return ones // 2 + 1
+
+    return 0
 
 
 def solution(content: str) -> int:
