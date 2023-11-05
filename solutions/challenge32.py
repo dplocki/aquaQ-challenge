b3570cead80a4bd9c6f5ecc2799d5d374e8335be
dplocki <dawid.plocki@gmail.com>
2023-11-05 09:36:11 +0100

    Solution 32: Black formating

--- solutions/challenge32.py
+++ solutions/challenge32.py
@@ -5,40 +5,37 @@ def is_braces_balanced(line: str) -> bool:
     stack = []
 
     for character in line:
-        if character in '([{':
+        if character in "([{":
             stack.append(character)
-        elif character in ')]}':
+        elif character in ")]}":
             if not stack:
                 return False
 
             previous_brace = stack.pop()
 
-            if previous_brace == '(' and character != ')':
+            if previous_brace == "(" and character != ")":
                 return False
 
-            if previous_brace == '[' and character != ']':
+            if previous_brace == "[" and character != "]":
                 return False
 
-            if previous_brace == '{' and character != '}':
+            if previous_brace == "{" and character != "}":
                 return False
 
     return len(stack) == 0
 
 
 def solution(content: str) -> int:
-    return sum(1
-            for line in content.splitlines()
-            if is_braces_balanced(line)
-        )
+    return sum(1 for line in content.splitlines() if is_braces_balanced(line))
 
 
-assert is_braces_balanced('()') == True
-assert is_braces_balanced('([]{})') == True
-assert is_braces_balanced('(a[b[]]c){}') == True
-assert is_braces_balanced(')()') == False
-assert is_braces_balanced('([a)]') == False
-assert is_braces_balanced(']{}[') == False
-assert is_braces_balanced('((a)){]') == False
+assert is_braces_balanced("()") == True
+assert is_braces_balanced("([]{})") == True
+assert is_braces_balanced("(a[b[]]c){}") == True
+assert is_braces_balanced(")()") == False
+assert is_braces_balanced("([a)]") == False
+assert is_braces_balanced("]{}[") == False
+assert is_braces_balanced("((a)){]") == False
 
 
-print('Solution', solution(get_file_content('input32.txt')))
+print("Solution", solution(get_file_content("input32.txt")))
