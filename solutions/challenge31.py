c2a8a4c573548b5a0b9457834451deb37df16d41
dplocki <dawid.plocki@gmail.com>
2023-11-05 09:36:03 +0100

    Solution 31: Black formating

--- solutions/challenge31.py
+++ solutions/challenge31.py
@@ -12,7 +12,7 @@ def split_the_input(line: str) -> Generator[str, None, None]:
 
     while index < limit:
         if index < limit - 1 and line[index + 1] == "'":
-            yield line[index:index + 2]
+            yield line[index : index + 2]
             index += 2
         else:
             yield line[index]
@@ -23,34 +23,41 @@ def solution(instructions: str) -> int:
     cube = Cube("222222222333111444666333111444666333111444666555555555")
 
     for instruction in split_the_input(instructions):
-        if instruction == 'F':
+        if instruction == "F":
             cube.F()
-        elif instruction == 'L':
+        elif instruction == "L":
             cube.L()
-        elif instruction == 'R':
+        elif instruction == "R":
             cube.R()
-        elif instruction == 'U':
+        elif instruction == "U":
             cube.U()
-        elif instruction == 'D':
+        elif instruction == "D":
             cube.D()
-        elif instruction == 'B':
+        elif instruction == "B":
             cube.B()
-        elif instruction == 'F\'':
+        elif instruction == "F'":
             cube.Fi()
-        elif instruction == 'L\'':
+        elif instruction == "L'":
             cube.Li()
-        elif instruction == 'R\'':
+        elif instruction == "R'":
             cube.Ri()
-        elif instruction == 'U\'':
+        elif instruction == "U'":
             cube.Ui()
-        elif instruction == 'D\'':
+        elif instruction == "D'":
             cube.Di()
-        elif instruction == 'B\'':
+        elif instruction == "B'":
             cube.Bi()
 
-    return reduce(mul, (int(p.colors[2]) for p in sorted(cube._face(FRONT), key=lambda p: (-p.pos.y, p.pos.x))), 1)
+    return reduce(
+        mul,
+        (
+            int(p.colors[2])
+            for p in sorted(cube._face(FRONT), key=lambda p: (-p.pos.y, p.pos.x))
+        ),
+        1,
+    )
 
 
 assert solution("U'LBRU") == 960
 
-print('Solution', solution(get_file_content('input31.txt')))
+print("Solution", solution(get_file_content("input31.txt")))
