2b664f3e221264716363ad026223dbfe73e568f5
dplocki <dawid.plocki@gmail.com>
2023-11-13 22:59:24 +0100

    Solution 37: Not working solution

--- solutions/challenge37.py
+++ solutions/challenge37.py
@@ -3,19 +3,21 @@ from utils import get_file_content, parse_as_csv_content
 from string import ascii_lowercase
 
 
-WORDS = set(word
-    for word in get_file_content("validwords15.txt").splitlines()
-    if len(word) == 5)
+WORDS = set(
+    word for word in get_file_content("validwords15.txt").splitlines() if len(word) == 5
+)
 
 
 def transformation(source):
     for row in source:
-        yield row[0], list(map(int, row[1].split(' ')))
+        yield row[0], list(map(int, row[1].split(" ")))
 
 
 def is_word_matching(word, allowed_letters):
-    return all(letter in allowed_letters
-        for letter, allowed_letters in zip(word, allowed_letters))
+    return all(
+        letter in allowed_letters
+        for letter, allowed_letters in zip(word, allowed_letters)
+    )
 
 
 def count_word_value(word):
@@ -24,29 +26,17 @@ def count_word_value(word):
     return sum(ord(character) - letter_a for character in word)
 
 
-def split_guess_per_word(source):
-    batch = []
-    previous = 0
+def find_the_words(source: List[Tuple[str, List[int]]]) -> str:
+    possibilities = None
 
-    for word, record in transformation(parse_as_csv_content(source)):
-        actual = sum(record)
-        if actual >= previous:
-            batch.append((word, record))
-            previous = actual
-        else:
-            yield batch
-            batch = [(word, record)]
-            previous = actual
+    for guess, score in transformation(parse_as_csv_content(source)):
+        print("\t", guess, score)
+        if not possibilities:
+            possibilities = WORDS.difference([guess])
+            allowed_letters = [set(ascii_lowercase) for _ in range(5)]
+            must_be_letters = set()
 
-    yield batch
-
-
-def find_the_word(batch: List[Tuple[str, List[int]]]) -> str:
-    possibilities = WORDS.difference(word for word, _ in batch)
-    allowed_letters = [set(ascii_lowercase) for _ in range(5)]
-    must_be_letters = set()
-
-    for guess, score in batch:
+        possibilities.difference_update([guess])
         for letter, value, index in zip(guess, score, range(5)):
             if value == 2:
                 allowed_letters[index] = set([letter])
@@ -61,25 +51,17 @@ def find_the_word(batch: List[Tuple[str, List[int]]]) -> str:
                 allowed_letters[index].remove(letter)
                 must_be_letters.add(letter)
 
-    possibilities = set(word
-        for word in possibilities
-        if is_word_matching(word, allowed_letters) and must_be_letters.issubset(word))
+        possibilities = set(
+            word for word in possibilities if is_word_matching(word, allowed_letters)
+        )
 
-    assert len(possibilities) == 1
-
-    return next(iter(possibilities))
+        if len(possibilities) == 1:
+            yield next(iter(possibilities))
+            possibilities = None
 
 
 def solution(content):
-    result = 0
-    for batch in split_guess_per_word(content):
-        print(batch)
-        result += count_word_value(find_the_word(batch))
-
-    return result
-
-    # return sum(
-    #     count_word_value(find_the_word(batch))
+    return sum(count_word_value(word) for word in find_the_words(content))
 
 
-solution(get_file_content('input37.csv'))
+print("Solution", solution(get_file_content("input37.csv")))
