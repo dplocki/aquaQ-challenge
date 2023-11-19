6c5a2ae7e95786d8a4d5a0f5f0f61fd78c731fe2
dplocki <dawid.plocki@gmail.com>
2023-11-19 11:40:32 +0100

    Solution 40: Add typing, add black formatting

--- solutions/challenge40.py
+++ solutions/challenge40.py
@@ -1,8 +1,9 @@
+from typing import Generator, Iterator, List
 from utils import get_file_content
 
 
-def find_extremum(content: str):
-    source = list(map(int, content.split(' ')))
+def find_extremum(content: str) -> Generator[int, None, None]:
+    source = list(map(int, content.split(" ")))
 
     for index in range(1, len(source) - 1):
         if source[index - 1] < source[index] > source[index + 1]:
@@ -12,25 +13,35 @@ def find_extremum(content: str):
             yield source[index]
 
 
-def take_until(function, iterable):
+def take_until(function: callable, iterable: Iterator) -> Generator:
     for i in iterable:
         yield i
         if not function(i):
             return
 
 
-def prominence(extremum_map, peak_index):
+def prominence(extremum_map: List[int], peak_index: int) -> int:
     peak = extremum_map[peak_index]
     left_result = 0
     right_result = 0
 
-    lutil = list(take_until(lambda p: p < peak, (extremum_map[index] for index in range(peak_index - 1, -1, -1))))
+    lutil = list(
+        take_until(
+            lambda p: p < peak,
+            (extremum_map[index] for index in range(peak_index - 1, -1, -1)),
+        )
+    )
     if lutil and lutil[-1] >= peak:
         left_result = peak - min(lutil)
     else:
         left_result = peak
 
-    rutil = list(take_until(lambda p: p < peak, (extremum_map[index] for index in range(peak_index + 1, len(extremum_map)))))
+    rutil = list(
+        take_until(
+            lambda p: p < peak,
+            (extremum_map[index] for index in range(peak_index + 1, len(extremum_map))),
+        )
+    )
     if rutil and rutil[-1] >= peak:
         right_result = peak - min(rutil)
     else:
@@ -42,15 +53,17 @@ def prominence(extremum_map, peak_index):
 def solution(content: str) -> int:
     extremum_map = list(find_extremum(content))
 
-    result = 0
-    for index in range(0, len(extremum_map), 2):
-        p = prominence(extremum_map, index)
-        result += p
-
-    return result
+    return sum(
+        prominence(extremum_map, index) for index in range(0, len(extremum_map), 2)
+    )
 
 
-assert solution('0 1 2 4 6 8 9 8 6 4 2 3 5 6 5 4 5 7 8 6 4 2 1 0') == 17
-assert solution('0 1 3 4 6 5 6 5 7 5 6 7 6 4 2 0 1 0 2 4 5 4 3 4 2 4 5 3 5 7 5 7 8 10 11 13 11 9 10 9 7 8 7 8 9 10 8 7 8 6 7 6 4 2 0') == 35
+assert solution("0 1 2 4 6 8 9 8 6 4 2 3 5 6 5 4 5 7 8 6 4 2 1 0") == 17
+assert (
+    solution(
+        "0 1 3 4 6 5 6 5 7 5 6 7 6 4 2 0 1 0 2 4 5 4 3 4 2 4 5 3 5 7 5 7 8 10 11 13 11 9 10 9 7 8 7 8 9 10 8 7 8 6 7 6 4 2 0"
+    )
+    == 35
+)
 
-print('Solution', solution(get_file_content('input40.txt')))
+print("Solution", solution(get_file_content("input40.txt")))
