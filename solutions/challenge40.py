444e704f2a0ba0abf7fd6a64b76ab0bb4ab258b6
dplocki <dawid.plocki@gmail.com>
2023-11-19 11:23:59 +0100

    Solution 40: Working solution

--- solutions/challenge40.py
+++ solutions/challenge40.py
@@ -1,3 +1,6 @@
+from utils import get_file_content
+
+
 def find_extremum(content: str):
     source = list(map(int, content.split(' ')))
 
@@ -21,13 +24,13 @@ def prominence(extremum_map, peak_index):
     left_result = 0
     right_result = 0
 
-    lutil = list(take_until(lambda p: p <= peak, (extremum_map[index] for index in range(peak_index - 1, -1, -1))))
+    lutil = list(take_until(lambda p: p < peak, (extremum_map[index] for index in range(peak_index - 1, -1, -1))))
     if lutil and lutil[-1] >= peak:
         left_result = peak - min(lutil)
     else:
         left_result = peak
 
-    rutil = list(take_until(lambda p: p <= peak, (extremum_map[index] for index in range(peak_index + 1, len(extremum_map)))))
+    rutil = list(take_until(lambda p: p < peak, (extremum_map[index] for index in range(peak_index + 1, len(extremum_map)))))
     if rutil and rutil[-1] >= peak:
         right_result = peak - min(rutil)
     else:
@@ -41,10 +44,13 @@ def solution(content: str) -> int:
 
     result = 0
     for index in range(0, len(extremum_map), 2):
-        result += prominence(extremum_map, index)
+        p = prominence(extremum_map, index)
+        result += p
 
     return result
 
 
 assert solution('0 1 2 4 6 8 9 8 6 4 2 3 5 6 5 4 5 7 8 6 4 2 1 0') == 17
 assert solution('0 1 3 4 6 5 6 5 7 5 6 7 6 4 2 0 1 0 2 4 5 4 3 4 2 4 5 3 5 7 5 7 8 10 11 13 11 9 10 9 7 8 7 8 9 10 8 7 8 6 7 6 4 2 0') == 35
+
+print('Solution', solution(get_file_content('input40.txt')))
